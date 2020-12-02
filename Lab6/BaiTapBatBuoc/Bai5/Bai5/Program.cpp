f6bae2721dc687006497f05e8a60f823d59a2f9e
La Quốc Thắng <quocthang0507@gmail.com>
2020-12-02 08:27:45 +0700

    Update program

--- Lab6/BaiTapBatBuoc/Bai5/Bai5/Program.cpp
+++ Lab6/BaiTapBatBuoc/Bai5/Bai5/Program.cpp
@@ -17,24 +17,17 @@ int main()
 void ChayChuongTrinh()
 {
 	char kt;
-	int Lich[6][7];
-	int thang, nam;
 	do
 	{
 		system("CLS");
-		cout<< "\nNhap thang = ";
-		cin >> thang;
-		cout << "\nNhap nam = ";
-		cin >> nam;
-		int thu=TimThu(thang, nam);
-		int ngay=TimSoNgay(thang, nam);
-		TaoLich(Lich,thu,ngay);
-		cout << "\n===========================================\n";
-		cout << "\nLICH THANG " << thang << " / " << nam << "\n\n";
-		XuatLich(Lich);
-		_getch();
+		int id1 = ChonTienTe();
+		int id2 = ChonTienTe();
+		double giaTri1, giaTri2;
+		cout << "Nhap gia tri: ";
+		cin >> giaTri1;
+		giaTri2 = DoiTienTe(id1, giaTri1, id2);
+		cout << giaTri1 << " " << TienTe[id1] << " bang " << giaTri2 << " " << TienTe[id2];
 		cout << "\n\nNhan ESC de THOAT, nhan phim bat ky de TIEP TUC!";
-		kt=_getch();
-	}
-	while (kt!=27);
+		kt = _getch();
+	} while (kt != 27);
 }
--- Lab6/BaiTapBatBuoc/Bai5/Bai5/Thuvien.h
+++ Lab6/BaiTapBatBuoc/Bai5/Bai5/Thuvien.h
@@ -1,89 +1,31 @@
-typedef int MaTran[6][7];
+﻿//Thu tu: USD,JPY,GBP,EUR,THB,CNY,KRW,RUB,INR
+string TienTe[9] = { "USD","JPY","GBP","EUR","THB","CNY","KRW","RUB","INR" };
+double GiaTri[9] = { 1,104.35,0.7504,0.8356,30.24,6.572,1107.95,76,73.6406 };
 
-int TimThu(int m, int y)
-{
-	int t, x, k, w;
-	t = y - (14 - m)/12;
-	x = t + t/4 - t/100 +t/400;
-	k = m + 12 * ((14 - m) / 12) - 2;
-	w = (1 + x + (31 * k) / 12) % 7;
-	return w;
+/*
+Đổi đơn vị tiền tệ
+*/
+double DoiTienTe(int id1, double value, int id2) {
+	return GiaTri[0] / GiaTri[id1] * value * GiaTri[id2];
 }
 
-int TimSoNgay(int m, int y)
-{
-	int soNgay;
-	switch (m)
+void XuatDanhSach() {
+	cout << "Danh sach tien te: " << endl;
+	for (int i = 0; i < 9; i++)
 	{
-		case 1:
-		case 3:
-		case 5:
-		case 7:
-		case 8:
-		case 10:
-		case 12:
-			soNgay = 31;
+		cout << i << ". " << TienTe[i] << endl;
+	}
+}
+
+int ChonTienTe() {
+	XuatDanhSach();
+	int i = -1;
+	while (true)
+	{
+		cout << "Nhap so thu tu: ";
+		cin >> i;
+		if (0 <= i && i <= 9)
 			break;
-		case 4:
-		case 6:
-		case 9:
-		case 11:
-			soNgay = 30;
-			break;
-		default:
-			if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
-				soNgay = 29;
-			else
-				soNgay = 28;
-				break;
 	}
-	return soNgay;
-}
-
-void TaoLich(MaTran Lich, int thu, int ngay)
-{
-	int dem=1;
-	//Gan gia tri dau tien cho ca ma tran la so 0
-	for (int i=0;i<6;i++)
-		for (int j=0;j<7;j++)
-			Lich[i][j]=0;
-	//Gan gia tri cho hang dau tien
-	for (int i=thu;i<7;i++)
-	{
-		Lich[0][i]=dem;
-		dem++;
-	}
-	//Gan gia tri cho cac hang con lai
-	for (int i=1;i<6;i++)
-		for (int j=0;j<7;j++)
-		{	
-			if (dem>ngay) break; //Neu vuot qua so ngay cua 1 thang thi THOAT
-			else
-			{
-				Lich[i][j]=dem;
-				dem++;
-			}
-		}
-}
-
-void XuatLich(MaTran Lich)
-{
-	cout << setw(10) << "Chu nhat"
-		<< setw(10) << "Thu 2"
-		<< setw(10) << "Thu 3"
-		<< setw(10) << "Thu 4"
-		<< setw(10) << "Thu 5"
-		<< setw(10) << "Thu 6"
-		<< setw(10) << "Thu 7" << "\n";
-	for (int i=0;i<6;i++)
-	{
-		cout << '\n';
-		for (int j=0;j<7;j++)
-		{
-			if(Lich[i][j]==0)
-				cout<< setw(10) << " ";
-			else
-			cout << setw(10) << Lich[i][j];
-		}
-	}
+	return i;
 }
